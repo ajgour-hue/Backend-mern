927daa120ed898c533d3c50e046dbdef90c4c5f8
Ajay Gour <theajaygour@gmail.com>
2026-01-28 21:33:52 +0530

    server file

--- day-4/server.js
+++ day-4/server.js
@@ -1,2 +1,9 @@
-console.log("hlloy");
-console.log("hlloy");
+// server ko statrt karna hain
+
+const app = require("./src/app");
+
+
+app.listen(3000,()=>{
+    console.log("server is in running mode");
+    })
+    
