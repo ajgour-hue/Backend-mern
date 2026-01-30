57a9d16e2535b460f0ab6d2b319a3e83343a1adc
Ajay Gour <theajaygour@gmail.com>
2026-01-30 06:01:12 +0530

    day-4

--- day-4/src/app.js
+++ day-4/src/app.js
@@ -25,6 +25,21 @@ app.post("/notes" , (req , res)=>{
 app.get("/notes" , (req , res)=>{
     res.send(notes)
 })
+// deletenotes 
+// params = > //delete/notes/0
+app.delete("/notes/:index", (req , res)=>{
+    // console.log(req.params);
+    delete notes[req.params.index]
+    res.send("node deleted as a succesfully")
+    
+})
 
+// updation partial to use hoga patch
+app.patch("/notes/:index" , (req , res)=>{
+
+    notes[req.params.index].description = req.body.description
+    res.send("node is to up date ")
+
+})
 
 module.exports = app;
