c440e79f34e29216a60652c100d6ec1aff7fb9f3
Ajay Gour <theajaygour@gmail.com>
2026-01-31 08:11:42 +0530

    6

--- day-6/server.js
+++ day-6/server.js
@@ -1,5 +1,20 @@
-const app = require("./src/app");
+// server ko statrt karna
+// database s connect karnaeka
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
-});
+
+// cluster = storage + processer 
+
+const app = require("./src/app")
+const monggose = require("mongoose")
+
+function connectToDb(){
+    monggose.connect("mongodb+srv://ajay:LdYvmpUm4DnepuaC@cluster0.d1ny8lk.mongodb.net/day-6").then(()=>{
+        console.log("connected to the database");
+    })
+}
+
+connectToDb()
+
+app.listen(3000,()=>{
+    console.log("server running");
+})
